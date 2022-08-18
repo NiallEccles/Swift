fbb26fb31d27d09c28b5784cc906555466b5328f
Niall Eccles <nialle@rothco.ie>
2022-08-18 21:38:24 +0100

    Created intro screen to explain what app does.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { IntroductionComponent } from './routes/introduction/introduction.component';
 import { SplashComponent } from './routes/splash/splash.component';
 
 const routes: Routes = [
   { path: '', component: SplashComponent },
+  { path: 'intro', component: IntroductionComponent },
   { path: 'chat', component: ChatWindowComponent }
 ];
 
--- src/app/routes/introduction/introduction.component.ts
+++ src/app/routes/introduction/introduction.component.ts
@@ -6,10 +6,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./introduction.component.scss']
 })
 export class IntroductionComponent implements OnInit {
+  public slideContent = [
+    {
+      title: 'Enter your name',
+      description: 'Let us know what to call you!'
+    },
+    {
+      title: 'Get connected',
+      description: 'You are randomly connected with another user who is currently online.'
+    },
+    {
+      title: 'Instant messaging',
+      description: 'Every character you enter is sent immediately to the person you are connected to. No need to hit send!'
+    }
+  ]
+  public currentItem: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public previousItem(){
+    if(this.currentItem - 1 <= 0){
+      this.currentItem = 0;
+    } else {
+      this.currentItem = this.currentItem - 1;
+    }
+  }
+
+  public nextItem(){
+    if(this.currentItem + 1 >= this.slideContent.length){
+      this.currentItem = this.slideContent.length - 1;
+    } else {
+      this.currentItem = this.currentItem + 1;
+    }
   }
 
 }
