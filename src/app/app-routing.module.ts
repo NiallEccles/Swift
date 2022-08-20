ffb4967d1ae004fb73893781aa874d63998e9d75
Niall Eccles <nialle@rothco.ie>
2022-08-20 22:19:44 +0100

    Created new route for chat.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ChatWindowComponent } from './components/chat-window/chat-window.component';
+import { ChatComponent } from './routes/chat/chat.component';
 import { EnterNameComponent } from './routes/enter-name/enter-name.component';
 import { IntroductionComponent } from './routes/introduction/introduction.component';
 import { SplashComponent } from './routes/splash/splash.component';
@@ -8,8 +9,8 @@ import { SplashComponent } from './routes/splash/splash.component';
 const routes: Routes = [
   { path: '', component: SplashComponent },
   { path: 'intro', component: IntroductionComponent },
-  { path: 'chat', component: ChatWindowComponent },
-  { path: 'enter-name', component: EnterNameComponent }
+  { path: 'chat', component: ChatComponent },
+  { path: 'enter-name', component: EnterNameComponent },
 ];
 
 @NgModule({
